b0a355d36a5a51e27615edd8008c5a2254232927
Emma Tang <tang1jo@gmail.com>
2020-09-13 21:30:46 -0700

    added antilibrary link, updated gitignore

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => (
     <div>
       <a href="https://twitter.com/emmaytang">twitter</a><br/>
       <a href="https://thoughtcloud.substack.com/">words</a><br/>
+      <a href="https://www.notion.so/antilibrary-3997863c457d479f926188af51edcdbd">antilibrary</a><br/>
       <a href="https://www.linkedin.com/in/emmaytang/">linkedin</a><br/>
     </div>
 
