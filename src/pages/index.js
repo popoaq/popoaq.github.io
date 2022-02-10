86537066364e99b5d9c9ba702d11d4ea7d47b298
Emma Tang <tang1jo@gmail.com>
2022-02-09 20:57:36 -0800

    update writing blog link to ghost

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <div style={{"font-size":"30px"}}>emma tang</div>
     <div>
       <a href="https://twitter.com/emmaytang">twitter</a><br/>
-      <a href="https://mirror.xyz/emmatang.eth">words</a><br/>
+      <a href="https://www.emmatang.xyz/">words</a><br/>
       <a href="https://acidic-dichondra-f1a.notion.site/antilibrary-3997863c457d479f926188af51edcdbd">antilibrary</a><br/>
       <a href="https://www.linkedin.com/in/emmaytang/">linkedin</a><br/>
     </div>
