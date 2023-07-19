4dcf4b75932609121c2d2391c9e4ff0e335e3a7e
Emma Tang <tang1jo@gmail.com>
2023-07-18 21:36:27 -0700

    add link to spiritual blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const IndexPage = () => (
       <a href="https://www.emmatang.xyz/">words</a><br/>
       <a href="https://acidic-dichondra-f1a.notion.site/antilibrary-3997863c457d479f926188af51edcdbd">antilibrary</a><br/>
       <a href="https://www.linkedin.com/in/emmaytang/">linkedin</a><br/>
+      <a href="https://monochico.substack.com/">spirit</a><br/>
     </div>
 
   </Layout>
